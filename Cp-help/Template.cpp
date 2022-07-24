ccfaa2be701943f47ab5e864b51fc6d52d9f3b7b
I-love-T <109909381+I-love-T@users.noreply.github.com>
2022-07-24 21:11:43 +0600

    Create Template.cpp

---
+++ Cp-help/Template.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pb push_back
+#define ll long long 
+#define vi vector<int> 
+#define vll vector<long long>
+#define endl "\n"
+#define FIO ios_base::sync_with_stdio(false)
+#define p(x); printf("%d",x);
+#define pl(x); printf("%lld",x);
+#define s(x); scanf("%d",&x);
+#define sl(x); scanf("%lld",&x);
+
+void solve() {
+	
+}
+int main() {
+	
+	
+	return 0;
+}
